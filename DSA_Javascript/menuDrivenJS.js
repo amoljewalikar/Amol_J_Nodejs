b0f7ca5a9ec534f21984b60f293d3bdaa504ea0b
amol jewalikar <62540556+amoljewalikar@users.noreply.github.com>
2024-07-17 18:02:51 +0530

    menu option code

---
+++ DSA_Javascript/menuDrivenJS.js
@@ -0,0 +1,51 @@
+const readline = require('readline');
+// Create interface for reading and writing to the console
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+// Function to display the menu
+function displayMenu() {
+  console.log('\n===== Menu =====');
+  console.log('1. Queue\n');
+  console.log('2. Stack\n');
+  console.log('3. Linked List\n');
+  console.log('4. Exit');
+}
+
+// Function to handle user input based on selected option
+function handleInput(option) {
+  switch (option) {
+    case '1':
+      console.log('\nYou have selected Queue.');
+      require('./dsaQueue');
+      break;
+    case '2':
+      console.log('\nYou have selected Stack.');
+      require('./dsaStack');
+      break;
+    case '3':
+      console.log('\nYou have selected Linked List.');
+      require('./dsaLinkedList');
+      break;
+    case '4':
+      console.log('Exiting the program.');
+      rl.close();
+      process.exit(0);
+    default:
+      console.log('Invalid option. Please select a valid option.');
+  }
+}
+
+// Function to prompt user for input
+function promptUser() {
+  rl.question('\nEnter your choice (1-4): ', (answer) => {
+    handleInput(answer);
+    promptUser(); // Continue prompting for input
+  });
+}
+
+// Initial display and start prompting for input
+displayMenu();
+promptUser();
